675379b89ed2d9ce18ac2f98ff4219d2b609fd74
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-10-10 22:52:12 +0600

    text added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home component</h2>
-        </div>
+
+        <section className="dark:bg-gray-800 dark:text-gray-100">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+                <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                    <h1 className="text-5xl font-bold leading-none sm:text-6xl  text-emerald-600">Let's Quiz </h1>
+                    <h2 className="text-5xl font-bold leading-none sm:text-6xl  text-emerald-600">Test your skills and become a master</h2>
+                    <p className="mt-6 mb-8 text-lg sm:mb-12 text-slate-500">We organize quizzes on various topics. Sign up if you haven't already and get access to millions of quizzes on the topic of your interest. <br /> <span className=' text-slate-800 font-semibold'>Start your journey here :</span>
+                    </p>
+                    <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                        <button rel="noopener noreferrer" className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100 bg-emerald-600  text-slate-800">Sign Up</button>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                    <img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+                </div>
+            </div>
+        </section>
+
     );
 };
 
