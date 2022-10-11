0384d555de9a37fea7ee5ef144130b7fb0ce9578
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-10-11 19:26:24 +0600

    Chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Statistics = () => {
+    const data = [
+        {
+            name: 'React',
+            total: 8,
+
+        },
+        {
+            name: 'JavaScript',
+            total: 9,
+
+        },
+        {
+            name: 'CSS',
+            total: 8,
+
+        },
+        {
+            name: 'Git',
+            total: 11,
+
+        },
+
+    ];
+
     return (
         <div>
-            <h2>This is statistics component</h2>
+            <LineChart className='m-auto mt-5' width={350} height={400} data={data} >
+                <Line type="monotone" dataKey="total" stroke="green" />
+                <XAxis dataKey="name" stroke='green' />
+                <YAxis stroke='green' />
+                <Tooltip></Tooltip>
+            </LineChart>
+
+            <h2 className='text-3xl mt-10 mb-8 font-semibold text-emerald-700'>Quiz Statistics</h2>
         </div>
     );
 };
